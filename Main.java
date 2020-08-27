77618cdbd2a2ee96cba1c949a6b7bc864f81703a
xtreme5351 <45394613+xtreme5351@users.noreply.github.com>
2020-08-27 21:11:09 +0100

    Final commit
    Just added notes lmao

--- Main.java
+++ Main.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 import com.sun.source.util.TaskEvent;
 
 public class Main{
+  //create the scanner class here so that it can be used through out the function
   static final Scanner userInput = new Scanner(System.in);
-
+  // a method to slice a given string into an amount of indexes
   private String slice(String str, int startingIndex, int endingIndex){
     if (startingIndex < 0){
       startingIndex = str.length() + startingIndex;
@@ -15,7 +16,7 @@ public class Main{
     }
     return str.substring(startingIndex, endingIndex);
   }
-
+  // a method to get the numbers into a 5 index list that can be called from and used for other funtions. The list is set to 1 to make the multiplication and division process easier.
   private int[] getNumbers(int userAnswer, String state){
     if(userAnswer > 5){
       System.out.println("Remember: max limit of numbers is 5");
@@ -30,7 +31,8 @@ public class Main{
     }
     return numbers;
   }
-
+  // all of the functions listed blow work in the same manner: they walk through the list and do whatever calculation they are meant to do.
+  // the add function
   private int add(int[] arr, int loops){
     int total = 0;
     for (int i: arr){
@@ -38,7 +40,7 @@ public class Main{
     }
     return total - (5 - loops);
   }
-
+  // the multiply function
   private int multiply(int[] arr){
     int total = 1;
     for (int i: arr){
@@ -46,7 +48,7 @@ public class Main{
     }
     return total;
   }
-
+  // the subtract function
   private int sub(int[] arr, int loops){
     int total = arr[0];
     for (int i: arr){
@@ -57,7 +59,7 @@ public class Main{
     total = (loops == 1) ? total + 1 : (total + (5 - loops));
     return total;
   }
-
+  // the division function
   private int div(int[] arr){
     int total = 0;
     for (int i = 0; i < 5; i++){
@@ -69,12 +71,12 @@ public class Main{
     }
     return total;
   }
-
+  // the start screen method
   private void start(){
     System.out.println("Welcome to calculator");
     System.out.println("Enter your calculation: ");
   }
-
+  // the end screen method that displays the result with the correct texts
   private void end(String slicedAnswer, int result){
     if (slicedAnswer == "div"){
       System.out.println("Note: This result below is an approximation");
@@ -83,18 +85,19 @@ public class Main{
       System.out.println("RESULT IS: " + result);
     }
   }
-
+  // the main funtion that does stuff
   public static void main(String[] args) {
+    // setup
     Main calc = new Main();
     calc.start();
     String answer = userInput.nextLine();
     System.out.println("Enter the amount of numbers to process [max 5]");
-
+    // calls the other funtions is is responsible for getting the numbers, while also checking if the result is a valid funtion i.e. it isnt a function like square root or something else
     int loopTimes = userInput.nextInt();
     String result = calc.slice(answer, 0, 3);
     int[] endCase = {0};
     int[] r = (result.equals("add") || result.equals("mul") || result.equals("div") || result.equals("sub")) ? calc.getNumbers(loopTimes, result) : endCase;
-
+    // just assigns a state to the correct function
     int finalResult = 0;
     if (result.equals("add") || result.equals("sum")){
       finalResult = calc.add(r, loopTimes);
